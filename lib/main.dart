00532de0614adc09478987bdce5081c940631719
Lucas Gabriel <90lucasgabriel@gmail.com>
2021-04-10 10:28:10 -0300

    04 - New: Interface and TextFields;

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,27 @@ Uri request = Uri.parse("https://api.hgbrasil.com/finance?key=d990ecda");
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.white38,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white38,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
   ));
 }
 
@@ -22,12 +43,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber[700],
       appBar: AppBar(
-        title: Text("Currency Converter"),
+        title: Text(
+          "Currency Converter",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
         shadowColor: Colors.transparent,
         backgroundColor: Colors.amber[700],
         centerTitle: true,
@@ -53,7 +82,73 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(color: Colors.green);
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(
+                        Icons.monetization_on_outlined,
+                        color: Colors.white,
+                        size: 150,
+                      ),
+                      Divider(
+                        height: 32,
+                        color: Colors.transparent,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: "Brazilian real",
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(),
+                          prefixText: "R\$ ",
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Divider(
+                        height: 16,
+                        color: Colors.transparent,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: "United States dollar",
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(),
+                          prefixText: "US\$ ",
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Divider(
+                        height: 16,
+                        color: Colors.transparent,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: "Euro",
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(),
+                          prefixText: "€ ",
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
               }
           }
         },
